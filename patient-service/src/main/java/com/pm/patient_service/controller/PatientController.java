f0a0d8755a2d17054921b71595af02c0093cff3d
Durosinlohun Uthman <udurosinlohun@gmail.com>
2025-06-18 02:38:31 +0100

    Created endpoint to create patient and implemented global exception handling for input validation

--- patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -1,12 +1,16 @@
 package com.pm.patient_service.controller;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.service.PatientService;
+import jakarta.validation.Valid;
 import org.apache.catalina.LifecycleState;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,8 +24,14 @@ public class PatientController {
         this.patientService = patientService;
     }
 
-    @GetMapping("/patients")
+    @GetMapping("/patient")
     ResponseEntity<List<PatientResponseDTO>> getPatients() {
         return ResponseEntity.ok().body(patientService.getPatients());
+    }
+
+    @PostMapping("/patient")
+    ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponse = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponse);
     }
 }
--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,10 +1,11 @@
 package com.pm.patient_service.service;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,5 +22,15 @@ public class PatientService {
         List<Patient> patients = patientRepository.findAll();
 
         return patients.stream().map(patient -> PatientMapper.toDto(patient)).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistException("Patient with email " + patientRequestDTO.getEmail() + " already exists");
+        }
+
+        Patient patient = PatientMapper.toEntity(patientRequestDTO);
+        Patient savedPatient =  patientRepository.save(patient);
+        return PatientMapper.toDto(savedPatient);
     }
 }
